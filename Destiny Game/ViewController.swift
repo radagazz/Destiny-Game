2ec8939f22c6b1276c512ba0b8a3423fbac6af1c
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-11-27 04:50:54 +0700

    Initial Commit

---
+++ Destiny Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destiny Game
+//
+//  Created by anurak teerarattananukulchai on 27/11/2563 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
