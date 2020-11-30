90ebd7b4e9657d920082b45f070024a3d0d84c59
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-11-30 11:34:03 +0700

    finished the project

--- Destiny Game/Controller/ViewController.swift
+++ Destiny Game/Controller/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    @objc func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        firstButton.setTitle(storyBrain.getChoice1(), for: .normal)
+        secondButton.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+    }
+    
 }
 
---
+++ Destiny Game/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  Destiny Game
+//
+//  Created by anurak teerarattananukulchai on 30/11/2563 BE.
+//
+
+import Foundation
+
+struct Story {
+    let title : String
+    let choice1 : String
+    let choice1Destination : Int
+    let choice2 : String
+    let choice2Destination: Int
+}
